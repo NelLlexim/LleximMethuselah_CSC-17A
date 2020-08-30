780e2ef3b1db2e93ff5c8a1fb657a3830a1f3213
nelllexim <methuselah.c.llexim@gmail.com>
2020-08-30 09:45:06 -0700

    Signed-off-by: nelllexim <methuselah.c.llexim@gmail.com>

---
+++ Lab/IntroLab/Hello World!/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: User
+ *
+ * Created on August 30, 2020, 9:17 AM
+ */
+
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Hello world!");
+
+    return 0;
+}
+
